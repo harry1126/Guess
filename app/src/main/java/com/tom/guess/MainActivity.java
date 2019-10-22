5583a636e0b8feddbbade75191998ab933063d84
miia <miia@com.tw>
2019-10-22 17:18:56 +0800

    second commit

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -8,27 +8,56 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    String counter;
+    int secret = new Random().nextInt(10)+1;
+    private ImageView result;
+    String TAG = MainActivity.class.getSimpleName();
+    private TextView guess;
+    private int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        guess = findViewById(R.id.guess_number);
+        result = findViewById(R.id.happy);
+        Log.d(TAG,"secret:"+secret);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
+    }       public void Button(View view){
+                num = Integer.parseInt(guess.getText().toString());
+                if(num ==secret){
+                Toast.makeText(MainActivity.this,"got it",Toast.LENGTH_LONG).show();
+                result.setImageResource(R.drawable.happy);
+                result.setVisibility(view.VISIBLE);
+                }else if (secret>num){
+                Toast.makeText(MainActivity.this,"bigger",Toast.LENGTH_LONG).show();
+                result.setImageResource(R.drawable.upset);
+                result.setVisibility(view.VISIBLE);
+
+                }else if (secret < num){
+                    Toast.makeText(MainActivity.this,"smaller",Toast.LENGTH_LONG).show();
+                    result.setImageResource(R.drawable.upset);
+                    result.setVisibility(view.VISIBLE);
+        }
     }
 
     @Override
